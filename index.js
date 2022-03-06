564842df5100d0eedb427b41b7f35356696336b9
jaydanasara <jaydanasara@yahoo.com>
2022-03-05 21:56:55 -0500

    still working on it

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 8080;
 const https=("https").default;
 const axios = require("axios");
 const cors = require("cors")
+// const bodyParser =require("body-parser")
 require("./db/index");
 
 const path =require('path')
@@ -18,12 +19,17 @@ const { env } = require("process");
 const base = "/api/v1"
 app.use(cors())
 app.use(express.json());
+
+
+//  app.use(bodyParser.urlencoded({extended:false}))
+//  app.use(bodyParser.json)
 app.use("/api/v1/users",users)
 app.use("/api/v1/products", products)
 app.use("/api/v1/admin", admin)
 app.use("/api/v1/profile", profile)
 app.use("/api/v1/homepage", homepage)
 app.use("/api/v1/orders",orders)
+app.use("/api/v1/checkout",checkout)
 app.use(express.static('public'))
 app.get('*',(req,res)=>{
     res.sendFile(path.join(__dirname,'public/index.html'))
